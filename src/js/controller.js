021b08722c5a68ac4f66366204b659c601efd118
dtrongtvo <tvdtrong@gmail.com>
2021-09-24 17:41:31 +0700

    finish app logic

---
+++ src/js/controller.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const billForm = document.querySelector("#bill");
+const customForm = document.querySelector("#custom-ratio");
+const peopleForm = document.querySelector("#people");
+const tipResult = document.querySelector(".result-number--tip");
+const totalResult = document.querySelector(".result-number--total");
+const btnsOption = document.querySelectorAll(".app-input-options button");
+const btnReset = document.querySelector(".app-btn--reset");
+
+let totalBill = 0;
+let tipRatio = 0;
+let numPeople = 1;
+let tipPerPerson;
+let totalPay;
+
+const calculateTip = function (totalBill, tipRatio, numPeople) {
+  return ((totalBill * (tipRatio / 100)) / numPeople).toFixed(2);
+};
+
+const calculatePersonTotal = function (totalBill, tipRatio, numPeople) {
+  return (
+    totalBill / numPeople +
+    parseInt(calculateTip(totalBill, tipRatio, numPeople))
+  ).toFixed(2);
+};
+
+const loadInput = function () {
+  totalBill = billForm.value && +billForm.value > 0 ? +billForm.value : 0;
+  numPeople =
+    peopleForm.value && +peopleForm.value >= 1 ? +peopleForm.value : 1;
+  // console.log(totalBill, numPeople);
+};
+
+const renderResults = function () {
+  tipResult.innerHTML = `$${tipPerPerson}`;
+  totalResult.innerHTML = `$${totalPay}`;
+};
+
+const calculateAndRender = function () {
+  tipPerPerson = calculateTip(totalBill, tipRatio, numPeople);
+  totalPay = calculatePersonTotal(totalBill, tipRatio, numPeople);
+  renderResults();
+};
+
+const reset = function () {
+  totalBill = 0;
+  tipRatio = 0;
+  numPeople = 1;
+  billForm.value = customForm.value = peopleForm.value = "";
+  calculateAndRender();
+};
+
+// When click ratio btn
+btnsOption.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    tipRatio = e.target.value;
+    calculateAndRender();
+  });
+});
+
+// When enter bill, ppl
+[billForm, peopleForm].forEach((el) => {
+  el.addEventListener("keyup", function () {
+    loadInput();
+    calculateAndRender();
+  });
+});
+
+// When enter custom ratio
+customForm.addEventListener("keyup", function () {
+  tipRatio = customForm.value && +customForm.value > 0 ? +customForm.value : 0;
+  calculateAndRender();
+});
+
+// When focus back on custom ratio after using ratio btn
+customForm.addEventListener("focus", function () {
+  if (customForm.value) {
+    tipRatio = customForm.value;
+    calculateAndRender();
+  }
+});
+
+btnReset.addEventListener("click", reset);
